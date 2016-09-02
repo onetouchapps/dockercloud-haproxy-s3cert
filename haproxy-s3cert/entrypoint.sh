eafef6eba31bff376b7c59451ecfe63bc320f4fd
Mark Henwood <mcbhenwood@gmail.com>
2016-09-02 16:22:51 +0100

    Convert wrapper to work with Docker Cloud HAProxy

--- haproxy-s3cert/entrypoint.sh
+++ haproxy-s3cert/entrypoint.sh
@@ -1,5 +1,5 @@
-#!/bin/bash
+#!/bin/sh
 set -e
 echo "Collecting cert pem from S3"
 export DEFAULT_SSL_CERT=`/usr/bin/python /haproxy-s3cert/collect_s3_cert.py`
-python /haproxy/main.py
+/usr/bin/dockercloud-haproxy
